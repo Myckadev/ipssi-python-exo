ec694dfedc8a96dfac32ce6a546b1027317e5841
Myckadev <schneider.myckael@gmail.com>
2024-04-15 15:38:25 +0200

    feat(ex9) : done c:

---
+++ ex9.py
@@ -0,0 +1,9 @@
+for fizzbuzz in range(51):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print('fizzbuzz')
+    elif fizzbuzz % 3 != 0 and fizzbuzz % 5 == 0:
+        print('buzz')
+    elif fizzbuzz % 3 == 0 and fizzbuzz % 5 != 0:
+        print('fizz')
+    else:
+        print(fizzbuzz)
